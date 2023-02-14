3bd60735939ee0d0fac6a146ba2f17a01fbe4ba2
Dick Larsson <dicklarsson@gmail.com>
2023-02-14 16:43:18 +0100

    Update example and add the transaction.js file from bitcoinjs-lib

--- signTransactionWithBitcoinjs.js
+++ signTransactionWithBitcoinjs.js
@@ -2,18 +2,40 @@ const bitcoin = require("bitcoinjs-lib");
 const coininfo = require("coininfo");
 const RAVENCOIN = coininfo.ravencoin.main.toBitcoinJS();
 
-const full = require("./mock/full.json").debug; 
-const txHex = full.rawUnsignedTransaction; 
+const full = require("./mock/full.json").debug;
+const UTXOs = full.rvnUTXOs.concat(full.assetUTXOs);
+
+const txHex = full.rawUnsignedTransaction;
 const tx = bitcoin.Transaction.fromHex(txHex);
 const txb = bitcoin.TransactionBuilder.fromTransaction(tx, RAVENCOIN);
+txb.UTXOs = UTXOs;
 
-// This assumes all inputs are spending utxos sent to the same Dogecoin P2PKH address (starts with D)
+//Very ugly but it works, set UTXOs as global variable so we can use it from the forked bitcoinjs-lib
+global.UTXOs = UTXOs;
 for (let i = 0; i < tx.ins.length; i++) {
   const address = full.inputs[i].address;
   const keyPair = getKeyPairByAddress(address);
   console.log(tx.ins[i].script.toString());
-  txb.sign(i, keyPair);
+
+ /*
+  interface TxbSignArg {
+    prevOutScriptType: string;
+    vin: number;
+    keyPair: Signer;
+    redeemScript?: Buffer;
+    hashType?: number;
+    witnessValue?: number;
+    witnessScript?: Buffer; 
+*/
+  const transactionBuilderSignArgument = {
+    prevOutScriptType: "p2pkh",
+    vin: i,
+    keyPair,
+  };
+  txb.sign(transactionBuilderSignArgument);
+ // txb.sign(i, keyPair);
 }
+
 const signedTxHex = txb.build().toHex();
 
 console.log(signedTxHex);
@@ -22,7 +44,7 @@ console.log(signedTxHex);
 //console.log(signedTxHex);
 
 function getKeyPairByAddress(address) {
-  const wif = full.privateKeys[address]; 
+  const wif = full.privateKeys[address];
   const keyPair = bitcoin.ECPair.fromWIF(wif, RAVENCOIN);
   return keyPair;
 }
